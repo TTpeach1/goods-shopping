1ca75d25a79e27b6aa27fa560abf31843f91508f
TTpeach1 <TTpeach1@Outlook.com>
2022-09-07 09:00:47 +0800

    完成了首页

---
+++ subpkgs/goods_detail/goods_detail.vue
@@ -0,0 +1,19 @@
+<template>
+  <view>
+    轮播图跳转页面
+  </view>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        
+      };
+    }
+  }
+</script>
+
+<style lang="scss">
+
+</style>
